5d83726100f0223225c6fe2fe46d8fc0905c3d05
luke-netwalker <luca.netwalker.code@gmail.com>
2023-06-18 12:11:00 +0200

    modifiche estetiche e correzione bug

--- script.js
+++ script.js
@@ -2940,7 +2940,7 @@ function showQuestion() {
         resultContainer.innerHTML = `Hai superato l'esame! Risposte corrette: ${correctAnswersCount}/${(lastQuestions-firstQuestionValue)} (${(correctAnswersCount / (lastQuestions - firstQuestionValue)) * 100}%)`;
     }
     else {
-        resultContainer.innerHTML = `Non hai superato l'esame! Risposte corrette: ${correctAnswersCount}/${(lastQuestions-firstQuestionValue)} (${(correctAnswersCount / (lastQuestions - firstQuestionValue)) * 100}%) per superare l'esame serve il 65% di risposte corrette`;
+        resultContainer.innerHTML = `Non hai superato l'esame! Risposte corrette: ${correctAnswersCount}/${(lastQuestions-firstQuestionValue)} (${(correctAnswersCount / (lastQuestions - firstQuestionValue)) * 100}%)<br> per superare l'esame serve il 65% di risposte corrette`;
     }
     progressBar.style.width = "100%";
     return;
